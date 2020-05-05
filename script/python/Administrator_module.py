484defe6db1602304eb7cf639416057f26ed2d93
qq1261275789 <60460280+qq1261275789@users.noreply.github.com>
2020-05-05 12:57:15 +0800

    Update Administrator_module.py

--- script/python/Administrator_module.py
+++ script/python/Administrator_module.py
@@ -6,6 +6,33 @@ from flask import Flask,request
 
 app = Flask(__name__)
 
+@app.route('/', methods=[ 'POST','GET'])
+def 查询1():
+    import pymysql
+    import time
+    # 创建数据库连接
+    config = {
+        'host': '139.196.203.66',
+        'port': 3306,
+        'user': 'root',
+        'passwd': '%E7%A0%81%E5%88%B0%E6%88%90%E5%8A%9F',
+        'db': 'CAMPUS_TRANSACTION_SQL',
+        'charset': 'utf8mb4'
+    }
+    db = pymysql.connect(**config)
+    # 初始化游标（创建游标）
+    cursor = db.cursor()
+    # 执行查询，并返回受影响的行数
+    cursor.execute("select * from COMMODITY")
+    # 返回所有的结果集 
+    result = cursor.fetchall()
+    para = []
+    for i in result:
+        text = {'id':i[0],'name':i[1],'password':i[2],'IS_PUTAWAY':i[6]}
+        para.append(text)
+    db.close()
+    return json.dumps(para, ensure_ascii=False, indent=4)
+
 @app.route('/Q_B_C_N/', methods=[ 'POST','GET'])
 def Find_item():   # Find_item = 查找商品        NameOrId = 商品名字或商品ID
     if request.method == "GET":
