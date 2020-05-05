882da9d415c5afe442f6ec181613aeb3b4d6f5e1
qq1261275789 <60460280+qq1261275789@users.noreply.github.com>
2020-05-05 16:07:21 +0800

    Update ceshi.py

--- script/python/ceshi.py
+++ script/python/ceshi.py
@@ -8,8 +8,30 @@ app = Flask(__name__)
 
 @app.route('/', methods=[ 'POST','GET'])
 def b():
-    bbb = request.args.get("bbb")
-    return bbb
+    import pymysql
+    import time
+    # 创建数据库连接
+    config = {
+        'host': '139.196.203.66',
+        'port': 3306,
+        'user': 'root',
+        'passwd': '%E7%A0%81%E5%88%B0%E6%88%90%E5%8A%9F',
+        'db': 'CAMPUS_TRANSACTION_SQL',
+        'charset': 'utf8mb4'
+    }
+    db = pymysql.connect(**config)
+    # 初始化游标（创建游标）
+    cursor = db.cursor()
+    # 执行查询，并返回受影响的行数
+    cursor.execute("select * from COMMODITY")
+    # 返回所有的结果集 
+    result = cursor.fetchall()
+    para = []
+    for i in result:
+        text = {'id':i[0],'name':i[1],'password':i[2],'IS_PUTAWAY':i[6]}
+        para.append(text)
+    db.close()
+    return json.dumps(para, ensure_ascii=False, indent=4)
 
 
 @app.route('/A/', methods=[ 'POST','GET'])
