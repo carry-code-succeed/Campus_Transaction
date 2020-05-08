167e941651ac2f00ee41c7b901c4362704a0cbad
qq1261275789 <60460280+qq1261275789@users.noreply.github.com>
2020-05-08 17:15:29 +0800

    Update start-CT.sh

--- script/sbin/start-CT.sh
+++ script/sbin/start-CT.sh
@@ -1,5 +1,5 @@
 python3 /root/Campus_Transaction/script/python/Administrator_module.py &
 python3 /root/Campus_Transaction/script/python/Upper_and_lower_rack_module.py &
-#python3 /root/Campus_Transaction/script/python/User_Registration_Login_module.py &
+python3 /root/Campus_Transaction/script/python/User_Registration_Login_module.py &
 #python3 /root/Campus_Transaction/script/python/ &
 #python3 /root/Campus_Transaction/script/python/ &
