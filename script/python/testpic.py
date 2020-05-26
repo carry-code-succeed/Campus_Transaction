4d30e3e663d484221807b1a4a50f6910e258fb89
qq1261275789 <60460280+qq1261275789@users.noreply.github.com>
2020-05-26 09:16:19 +0800

    Update testpic.py

--- script/python/testpic.py
+++ script/python/testpic.py
@@ -46,6 +46,6 @@ def Save_picture():
     return jieguo
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=6178,
+    app.run(host='127.0.0.1', port=6109,
             ssl_context = 'adhoc'
            )
