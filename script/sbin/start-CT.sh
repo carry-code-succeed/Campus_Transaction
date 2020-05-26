1f4bbf0ae00802d9ee85c559840e0370e4770d97
qq1261275789 <60460280+qq1261275789@users.noreply.github.com>
2020-05-26 09:57:23 +0800

    Update start-CT.sh

--- script/sbin/start-CT.sh
+++ script/sbin/start-CT.sh
@@ -2,3 +2,4 @@ nohup python3 -u /root/Campus_Transaction/script/python/Administrator_module.py 
 nohup python3 -u /root/Campus_Transaction/script/python/Upper_and_lower_rack_module.py > /root/CAMPUS_TRANSACTION/log/Upper_and_lower_rack_module.log 2>&1 &
 nohup python3 -u /root/Campus_Transaction/script/python/User_Registration_Login_module.py > /root/CAMPUS_TRANSACTION/log/User_Registration_Login_module.log 2>&1 &
 nohup python3 -u /root/Campus_Transaction/script/python/Query_module.py > /root/CAMPUS_TRANSACTION/log/Query_module.log 2>&1 &
+nohup python3 -u /root/Campus_Transaction/script/python/testpic.py > /root/CAMPUS_TRANSACTION/log/testpic.log 2>&1 &
