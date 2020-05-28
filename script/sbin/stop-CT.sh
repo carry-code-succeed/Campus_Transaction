bc1c6aec372d975aa700d631fdf560fb4491c4aa
qq1261275789 <60460280+qq1261275789@users.noreply.github.com>
2020-05-28 09:34:56 +0800

    Update stop-CT.sh

--- script/sbin/stop-CT.sh
+++ script/sbin/stop-CT.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
+# 关闭模块
+echo "-------查找模块进程--------"
 NAME=python3
 echo $NAME
 ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
 echo $ID
-echo "-------分割线--------"
+echo "-------关闭模块--------"
 for id in $ID
 do
 kill -9 $id
 echo "killed $id"
 done
-echo "-------分割线--------"
+echo "-------关闭完成--------"
