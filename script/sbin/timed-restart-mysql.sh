3e70e206432dac6297438c04bf749507c762b65f
qq1261275789 <60460280+qq1261275789@users.noreply.github.com>
2020-05-28 13:36:27 +0800

    Update timed-restart-mysql.sh

--- script/sbin/timed-restart-mysql.sh
+++ script/sbin/timed-restart-mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # 定时重启mysql和模块功能
-declare -i i
-i=1
-while [ "1" != "0" ];do
-	echo "第$i次重启"
-	i=i+1
-	echo `service mysql restart`
-	echo `date +%Y-%m-%d-%X`
-	restart-CT.sh
+# declare -i i
+# i=1
+# while [ "1" != "0" ];do
+# 	echo "第$i次重启"
+# 	i=i+1
+# 	echo `service mysql restart`
+# 	echo `date +%Y-%m-%d-%X`
+# 	restart-CT.sh
 #	定时30分钟
-	sleep 30m
-done
+# 	sleep 30m
+# done
